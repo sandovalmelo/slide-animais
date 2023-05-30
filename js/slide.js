8c0b83d41f3a40c14a8ce07975efa2a4ba3e5a0c
sandovalmelo <juniormelo777@gmail.com>
2023-05-30 18:17:28 -0300

    add Slide constructor

--- js/slide.js
+++ js/slide.js
@@ -1,1 +1,6 @@
-export default class Slide {}
+export default class Slide {
+	constructor(slide, wrarpper) {
+		this.slide = document.querySelector(slide);
+		this.wrarpper = document.querySelector(wrarpper);
+	}
+}
